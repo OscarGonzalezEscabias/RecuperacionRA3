4e2124d60e70d2d57fa2761461cefcdbdc77592e
darioromeroromera <darioromeroromera@gmail.com>
2023-06-14 13:25:00 +0200

    1.0.0 Test creados

---
+++ Recuperacion_RA3_RA4Test.java
@@ -0,0 +1,41 @@
+
+public class Recuperacion_RA3_RA4Test {
+    public static void main(String[] args) {
+        testGenerarPrimosPositivos();
+        testGenerarPrimosNegativos();
+        testGenerarPrimoCero();
+    }
+
+    public static void testGenerarPrimosPositivos() {
+        int max = 3;
+        int[] expected = {2};
+        int[] actual = Recuperacion_RA3_RA4.generarPrimos(max);
+        assertArrayEquals(expected, actual);
+        System.out.println("Test 'testGenerarPrimosPositivos' pasado");
+    }
+    public static void testGenerarPrimosNegativos() {
+        int max = -3;
+        int[] expected = {};
+        int[] actual = Recuperacion_RA3_RA4.generarPrimos(max);
+        assertArrayEquals(expected, actual);
+        System.out.println("Test 'testGenerarPrimosNegativos' pasado");
+    }
+    public static void testGenerarPrimoCero() {
+        int max = 0;
+        int[] expected = {};
+        int[] actual = Recuperacion_RA3_RA4.generarPrimos(max);
+        assertArrayEquals(expected, actual);
+        System.out.println("Test 'testGenerarPrimoCero' pasado");
+    }
+
+    public static void assertArrayEquals(int[] expected, int[] actual) {
+        if (expected.length != actual.length) {
+            return;
+        }
+        for (int i = 0; i < expected.length; i++) {
+            if (expected[i] != actual[i]) {
+                return;
+            }
+        }
+    }
+}
---
+++ out/production/RecuperacionRA3/README.md
@@ -0,0 +1,1 @@
+# RecuperacionRA3
